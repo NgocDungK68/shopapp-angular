ac1710802c1fe4b58bca043f5b2a98ba325f1f6f
NgocDungK68 <dungtranngoc1412@gmail.com>
2025-12-18 03:02:57 +0700

    feat:: product admin

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -4,11 +4,14 @@ import { Observable } from "rxjs";
 import { environment } from "../environments/environment";
 import { Product } from "../models/product";
 import { ApiResponse } from "../responses/api.response";
+import { InsertProductDTO } from "../dtos/product/insert.product.dto";
+import { UpdateProductDTO } from "../dtos/product/update.product.dto";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiBaseUrl = environment.apiBaseUrl;
   private apiGetProducts = `${environment.apiBaseUrl}/products`;
 
   constructor(private http: HttpClient) { }
@@ -37,5 +40,33 @@ export class ProductService {
     debugger
     const params = new HttpParams().set('ids', productIds.join(','));
     return this.http.get<ApiResponse>(`${this.apiGetProducts}/by-ids`, { params });
+  }
+
+  deleteProduct(productId: number): Observable<ApiResponse> {
+    debugger
+    return this.http.delete<ApiResponse>(`${this.apiBaseUrl}/products/${productId}`);
+  }
+
+  updateProduct(productId: number, updatedProduct: UpdateProductDTO): Observable<ApiResponse> {
+    return this.http.put<ApiResponse>(`${this.apiBaseUrl}/products/${productId}`, updatedProduct);
+  }  
+
+  insertProduct(insertProductDTO: InsertProductDTO): Observable<ApiResponse> {
+    // Add a new product
+    return this.http.post<ApiResponse>(`${this.apiBaseUrl}/products`, insertProductDTO);
+  }
+
+  uploadImages(productId: number, files: File[]): Observable<ApiResponse> {
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append('files', files[i]);
+    }
+    // Upload images for the specified product id
+    return this.http.post<ApiResponse>(`${this.apiBaseUrl}/products/uploads/${productId}`, formData);
+  }
+
+  deleteProductImage(id: number): Observable<any> {
+    debugger
+    return this.http.delete<string>(`${this.apiBaseUrl}/product_images/${id}`);
   }
 }
